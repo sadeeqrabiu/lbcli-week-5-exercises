d800b7dbf92385ba53e539b218098f527d95ded7
Abubakar Sadiq <53898703+sadeeqrabiu@users.noreply.github.com>
2026-04-10 12:34:26 +0100

    quick fix

--- submission/05.sh
+++ submission/05.sh
@@ -1,3 +1,3 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-echo "029600b2752102e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277ac"
+echo "029600b27576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac"
